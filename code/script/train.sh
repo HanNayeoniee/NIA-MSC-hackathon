a090d70ef3b43a34c83938c8cdad16e727200c86
Na Yeon Han <piggutail79@gmail.com>
2022-12-20 11:41:57 +0900

    Update train.sh

--- code/script/train.sh
+++ code/script/train.sh
@@ -1,19 +1,19 @@
 export CUDA_VISIBLE_DEVICES=0,1,2
 export CUDA_DEVICE_ORDER=PCI_BUS_ID
+export GIT_PYTHON_REFRESH=quiet
 
 ###### 경로 수정
-PRETRAIN_MODEL=../model/etri_t5
+PRETRAIN_MODEL=/workspace/model/etri_t5
 
-DATASET_DIR=../dataset/nia_dataset/
-TRAIN_PATH=$DATASET_DIR/train_memory2.jsonl
-DEV_PATH=$DATASET_DIR/valid_memory2.jsonl
+DATASET_DIR=/workspace/data/
+TRAIN_PATH=$DATASET_DIR/train.jsonl
+DEV_PATH=$DATASET_DIR/valid.jsonl
 
 
 ###### 경로 수정
-TRAIN_OUTPUT_PATH=../model/1219_memory/ 
+TRAIN_OUTPUT_PATH=/workspace/model/new_model/ 
 
 mkdir $TRAIN_OUTPUT_PATH
-
 
 cd ..
 pwd
@@ -30,9 +30,8 @@ python train.py \
 --per_device_train_batch_size 12 \
 --gradient_accumulation_steps 1 \
 --num_train_epochs 3.0 \
---n_gpu 3 \
+--n_gpu 2 \
 --parallel_mode ParallelMode.NOT_DISTRIBUTED \
 --train_dataloader random \
 --do_predict \
 --predict_with_generate
-# popd
